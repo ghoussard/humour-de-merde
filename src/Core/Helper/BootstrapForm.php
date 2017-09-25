a9c7d555f0f7adac0625ab75ab5b4cc5aac03428
ghoussard <houssard.gregoire@gmail.com>
2017-09-25 09:45:10 +0200

    Refactoring BootstrapForm

--- src/Core/Helper/BootstrapForm.php
+++ src/Core/Helper/BootstrapForm.php
@@ -23,19 +23,6 @@ class BootstrapForm {
 
 
     /**
-     * Retourne une valeur des données générées par le formulaire
-     * @param string $key
-     * @return mixed
-     */
-    private function getValue(string $key) {
-        if(isset($this->data[$key])) {
-            return $this->data[$key];
-        }
-        return null;
-    }
-
-
-    /**
      * Retourne un input
      * @param string $name
      * @param string $label
@@ -47,18 +34,18 @@ class BootstrapForm {
             $options['type'] = 'text';
         }
 
-        $params = "";
-        foreach ($options as $param => $value) {
-            $params .= "{$param}=\"{$value}\" ";
+        $params = $this->extractOptions($options);
+
+        $value = "";
+        if(!empty($this->getValue($name))) {
+            $value = 'value="' . $this->getValue($name) . '" ';
         }
 
-        $html = "<label for=\"{$name}\">{$label}</label>";
-        $html .= "<input class=\"form-control\" ";
-        $html .= "name=\"{$name}\" id=\"{$name}\" ";
-        $html .= "{$params} value=\"{$this->getValue($name)}\"></input>";
-        $html .= $this->writeError($name);
+        $html = $this->getLabel($name, $label);
+        $html .= "<input class=\"form-control\" name=\"{$name}\" id=\"{$name}\" ";
+        $html .= "{$params} {$value}></input>";
 
-        return $this->surround($html, $this->isValid($name));
+        return $this->surround($html, $name);
     }
 
 
@@ -71,12 +58,9 @@ class BootstrapForm {
      * @return string
      */
     public function select(string $name, string $label, array $data = [], array $options = []): string {
-        $params = "";
-        foreach ($options as $param => $value) {
-            $params .= "{$param}=\"{$value}\" ";
-        }
+        $params = $this->extractOptions($options);
 
-        $html = "<label for=\"{$name}\">{$label}</label>";
+        $html = $this->getLabel($name, $label);
         $html .= "<select class=\"form-control\" ";
         $html .= "name=\"{$name}\" id=\"{$name}\" {$params}>";
         foreach ($data as $item) {
@@ -84,9 +68,8 @@ class BootstrapForm {
             $html .= "<option value=\"{$item->id}\" {$selected}>{$item->name}</option>";
         }
         $html .= "</select>";
-        $html .= $this->writeError($name);
 
-        return $this->surround($html, $this->isValid($name));
+        return $this->surround($html, $name);
     }
 
 
@@ -98,18 +81,14 @@ class BootstrapForm {
      * @return string
      */
     public function textarea(string $name, string $label, array $options = ['rows' => '3']): string {
-        $params = "";
-        foreach ($options as $param => $value) {
-            $params .= "{$param}=\"{$value}\" ";
-        }
+        $params = $this->extractOptions($options);
 
-        $html = "<label for=\"{$name}\">{$label}</label>";
+        $html = $this->getLabel($name, $label);
         $html .= "<textarea class=\"form-control\" ";
         $html .= "name=\"{$name}\" id=\"{$name}\" ";
         $html .= "{$params}>{$this->getValue($name)}</textarea>";
-        $html .= $this->writeError($name);
 
-        return $this->surround($html, $this->isValid($name));
+        return $this->surround($html, $name);
     }
 
 
@@ -123,17 +102,18 @@ class BootstrapForm {
 
 
     /**
-     * Entoure un champ de la balise adaptée et le retourne
+     * Entoure un champ de la balise adaptée et d'une erreur s'il y en a une et le retourne
      * @param string $html
-     * @param bool $valid
+     * @param string $name
      * @param string $class
      * @return string
+     * @internal param bool $valid
      */
-    private function surround(string $html, bool $valid, string $class = 'form-group'): string {
-        if(!$valid) {
-            return '<div class="'. $class . ' has-danger">' . $html . '</div>';
+    private function surround(string $html, string $name, string $class = 'form-group'): string {
+        if($this->isValid($name)) {
+            return '<div class="' . $class . '">' . $html . '</div>';
         }
-        return '<div class="'. $class . '">' . $html . '</div>';
+        return '<div class="' . $class . '">' . $html . $this->writeError($name) . '</div>';
     }
 
 
@@ -143,11 +123,7 @@ class BootstrapForm {
      * @return string
      */
     private function writeError(string $name): string {
-        if(!$this->isValid($name)) {
-            $html = "<small class=\"form-text text-muted\">{$this->getError($name)}</small>";
-            return $html;
-        }
-        return "";
+        return "<small class=\"form-text text-muted\">{$this->getError($name)}</small>";
     }
 
 
@@ -162,12 +138,50 @@ class BootstrapForm {
 
 
     /**
-     * Retourne l'erreur associée à un champ
+     * Retourne une valeur des données générées par le formulaire
+     * @param string $key
+     * @return string|null
+     */
+    private function getValue(string $key): ?string {
+        if(isset($this->data[$key])) {
+            return $this->data[$key];
+        }
+        return null;
+    }
+
+
+    /**
+     * Retourne un label
      * @param string $name
+     * @param string $label
      * @return string
      */
-    private function getError(string $name): string {
+    private function getLabel(string $name, string $label): string {
+        return "<label for=\"{$name}\">{$label}</label>";
+    }
+
+
+    /**
+     * Retourne l'erreur associée à un champ
+     * @param string $name
+     * @return string|null
+     */
+    private function getError(string $name): ?string {
         return $this->errors[$name];
+    }
+
+
+    /**
+     * Extraie les options
+     * @param array $options
+     * @return string
+     */
+    private function extractOptions(array $options): string {
+        $params = "";
+        foreach ($options as $param => $value) {
+            $params .= "{$param}=\"{$value}\" ";
+        }
+        return $params;
     }
 
 }
